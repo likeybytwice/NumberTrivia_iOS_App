e469407869c55b673925dd06d9916476ebf44518
mayla ikiz <mayla.mira@hotmail.com>
2018-12-02 22:52:17 +0100

    Initial Commit

---
+++ NumberTrivia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NumberTrivia
+//
+//  Created by Mayla on 02/12/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
